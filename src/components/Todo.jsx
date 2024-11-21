ea15efd258be8d6779ee38b58732c6d003b1dfe0
hyeonky <hyeon2762@naver.com>
2024-11-21 15:30:35 +0900

    mockTodoDate 생성

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,6 +3,27 @@ import TodoList from './TodoList';
 import TodoEditor from './TodoEditor';
 import TodoHd from './TodoHd';
 
+const mockTodoDate = [
+  {
+    id: 1,
+    isDone: false,
+    task: '고양이 밥주기',
+    createDate: '2024.04.22',
+  },
+  {
+    id: 2,
+    isDone: false,
+    task: '감자, 맛동산 캐기',
+    createDate: '2024.04.22',
+  },
+  {
+    id: 3,
+    isDone: false,
+    task: '고양이 놀아주기',
+    createDate: '2024.04.22',
+  },
+];
+
 const Todo = () => {
   return (
     <div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,16 +7,31 @@ const TodoList = () => {
       <input type="text" />
       <ul>
         <li>
+          <input
+            type="checkbox"
+            name=""
+            id=""
+          />
           <strong>고양이 밥주기</strong>
           <span>2024.04.22</span>
           <button>삭제</button>
         </li>
         <li>
+          <input
+            type="checkbox"
+            name=""
+            id=""
+          />
           <strong>감자, 맛동산 캐기</strong>
           <span>2024.04.22</span>
           <button>삭제</button>
         </li>
         <li>
+          <input
+            type="checkbox"
+            name=""
+            id=""
+          />
           <strong>고양이 놀아주기</strong>
           <span>2024.04.22</span>
           <button>삭제</button>
