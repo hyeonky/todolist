7d2a2d3ef1d12f4b7c70ed3d0f28b3979c20cf96
hyeonky <hyeon2762@naver.com>
2024-11-21 15:04:42 +0900

    todo import

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import TodoList from './TodoList';
+import TodoEditor from './TodoEditor';
+import TodoHd from './TodoHd';
 
 const Todo = () => {
-  return <div></div>;
+  return (
+    <div>
+      <TodoHd />
+      <TodoEditor />
+      <TodoList />
+    </div>
+  );
 };
 
 export default Todo;
